ca8545d45becf410f4faeb289c88565c4655f79d
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-10-09 21:41:03 +0300

    adding votes yes and votes no

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :votes
+
+	def votes_yes
+		self.votes.select { |vote| vote.status == 'yes' }
+	end
+
+	def votes_no
+		self.votes.select { |vote| vote.status == 'no' }
+	end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,25 @@ RSpec.describe Post, :type => :model do
 	context "associations" do
 		it { should belong_to(:user) }
 		it { should have_many(:votes) }
+
+		context 'yes no' do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:user2) { FactoryGirl.create(:user2) }
+			let(:post) { FactoryGirl.create(:post) }
+
+			it 'has many votes_yes' do
+				votey = user.votes.create(post_id: post.id, status: 'yes')
+				voten = user2.votes.create(post_id: post.id, status: 'no')
+				expect(post.votes_yes).to include votey
+				expect(post.votes_yes).to_not include voten
+			end
+
+			it 'has many votes_no' do
+				votey = user.votes.create(post_id: post.id, status: 'yes')
+				voten = user2.votes.create(post_id: post.id, status: 'no')
+				expect(post.votes_no).to include voten
+				expect(post.votes_no).to_not include votey
+			end
+		end
 	end
 end
