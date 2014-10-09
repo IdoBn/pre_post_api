c98b43c25566e9407004c400650c0bef61865f3b
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-10-09 19:59:49 +0300

    more refactoring

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Vote, :type => :model do
 	context 'validations' do
 		it { should validate_presence_of(:status) }
-
 		it { expect(FactoryGirl.build(:vote, status: 'no')).to be_valid }
 		it { expect(FactoryGirl.build(:vote, status: 'yes')).to be_valid }
 		it { expect(FactoryGirl.build(:vote, status: 'dodo')).to_not be_valid }
