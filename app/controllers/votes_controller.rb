7f89053002cf1ec44a50fc3d0a7a0f51a6bd79c1
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-10-09 23:04:49 +0300

    refactoring votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,12 @@
 class VotesController < ApplicationController
 	def yes
 		@vote = Vote.new(post_id: params[:post_id], status: 'yes', user_id: current_user.id)
-		if @vote.save
-			render json: { vote: @vote }
-		else
-			render json: {errors: @vote.errors.full_messages}
-		end
+		render_vote(@vote.save)
 	end
 
 	def no
 		@vote = Vote.new(post_id: params[:post_id], status: 'no', user_id: current_user.id)
-		if @vote.save
-			render json: { vote: @vote }
-		else
-			render json: {errors: @vote.errors.full_messages}
-		end
+		render_vote(@vote.save)
 	end
 
  private
